dd6891e7ea833f6a734b519e2bfe5c3c2dab9022
AlcorBlackCat <kougetu0game@gmail.com>
2023-06-12 13:44:22 +0900

    i

--- grid_wilcoxon.py
+++ grid_wilcoxon.py
@@ -20,8 +20,8 @@ count = 0
 number_of_cars = 300
 half_number_of_cars = int(number_of_cars/2)
 
-for file_name1 in os.listdir(dir1): #ファイルディレクトリ「dir1」の中身のリストをファイル名で取り出す→file_name1に格納？
-    file_path1 = os.path.join(dir1,file_name1) #dir1/file_name1というパスを作る
+for file_name1 in os.listdir(dir1): #ファイルディレクトリ「dir1」の中身をファイル、ディレクトリ問わず取り出す→ 配列「file_name1」に格納？
+    file_path1 = os.path.join(dir1,file_name1) #上で取り出したものについて、それぞれdir1/file_name1というパスを作る？
     #print(file_path1)
 
     duration_list1 = []
@@ -37,13 +37,13 @@ for file_name1 in os.listdir(dir1): #ファイルディレクトリ「dir1」の
 
     duration_list1_sorted = sorted(duration_list1)[int(len(duration_list1)/2):] #duration_list1をソートした後半をduration_list1_sortedとする
     distance_list1_sorted = sorted(distance_list1)[int(len(duration_list1)/2):]
-    duration_list_sorted1 = [] #初期化
+    duration_list_sorted1 = [] #配列の生成、初期化
     distance_list_sorted1 = []
     for i in range(half_number_of_cars): #half_number_of_carsの数値分の回数以下の処理を繰り返す
-        duration_list_sorted1.append((np.random.choice(duration_list1_sorted)))
+        duration_list_sorted1.append((np.random.choice(duration_list1_sorted))) #すぐ上で生成された配列に、上で後半をソートして抽出した配列からランダムに要素を追加する
         distance_list_sorted1.append((np.random.choice(distance_list1_sorted)))
 
-    file_name2 = os.listdir(dir2)[count]
+    file_name2 = os.listdir(dir2)[count] #所々違うけれど概ね27行目以降と同じ？
     file_path2 = os.path.join(dir2,file_name2)
     #print(file_path2)
 
@@ -66,30 +66,30 @@ for file_name1 in os.listdir(dir1): #ファイルディレクトリ「dir1」の
         duration_list_sorted2.append((np.random.choice(duration_list2_sorted)))
         distance_list_sorted2.append((np.random.choice(distance_list2_sorted)))
 
-    U = 0
+    U = 0 #ここから違う処理
     for j in range(len(duration_list_sorted2)):
         for k in range(len(duration_list_sorted1)):
             if duration_list_sorted2[j] > duration_list_sorted1[k]:
                 U += 1
-    U_duration_list.append(U)
+    U_duration_list.append(U) #Uはウィルコクソンの順位和検定における転倒数？　一番最初に生成しておいた配列にUを格納
 
     U = 0
     for j in range(len(distance_list_sorted2)):
         for k in range(len(distance_list_sorted1)):
             if distance_list_sorted2[j] > distance_list_sorted1[k]:
                 U += 1
-    U_distance_list.append(U)
+    U_distance_list.append(U) #こっちは移動距離
 
     count += 1
 
 #print(U_duration_list)
 #print(U_distance_list)
-U_duration_avg = np.mean(U_duration_list)
+U_duration_avg = np.mean(U_duration_list) #U_duration_listの平均を算出
 U_distance_avg = np.mean(U_distance_list)
-U_duration_std = np.std(U_duration_list)
+U_duration_std = np.std(U_duration_list) #U_duration_listの標準偏差を算出
 U_distance_std = np.std(U_distance_list)
 print("転倒数の平均")
-print("duration : " + str(int(U_duration_avg)))
+print("duration : " + str(int(U_duration_avg))) #int型のU_duration_avgをstr(string？多分文字列)型に変換して出力
 print("distance : " + str(int(U_distance_avg)))
 print("転倒数の標準偏差")
 print("duration : " + str(int(U_duration_std)))

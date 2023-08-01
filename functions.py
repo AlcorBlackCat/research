82db331f86b8741acb349fd8d29ea0f5c0e3f3d4
AlcorBlackCat <kougetu0game@gmail.com>
2023-08-01 15:19:11 +0900

    i

--- functions.py
+++ functions.py
@@ -3,3 +3,28 @@
 
 #関数を以下に集約
 #目次
+#8 グローバル変数
+
+number_of_cars = 300 
+number_of_obstacles = 10 
+number_of_fake_cars = 1 
+number_of_fake_obstacles = 1 
+opportunistic_communication_rate = 1.0  
+sensitivity = 1.0
+
+obstacles_list = []
+fakeobs_list = []
+obstacle_node_id_list = []
+fakeobs_node_id_list = []
+pair_node_id_list = []
+fakepair_node_id_list = []
+cars_list = []
+fakecars_list = []
+obstacle_dic = {}
+
+goal_time_list = []
+number_of_shortest_path_changes_list = []
+number_of_opportunistic_communication_list = []
+moving_distance_list = []
+time_list = []
+
